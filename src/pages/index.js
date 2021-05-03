b6f5d3b5c14c5bf2a4e5a168faf6d1c0ed9d6f3d
Pitt Wu <kgb00128@gmail.com>
2021-05-03 15:26:32 +0800

    feat：init gatsby blog

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Seo from '../components/seo';
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>Home</h1>
+  </Layout>
+);
+
+export default IndexPage;
